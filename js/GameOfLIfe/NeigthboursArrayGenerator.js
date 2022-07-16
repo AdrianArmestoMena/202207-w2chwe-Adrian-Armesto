158c92fee30a090ea1272ddc414c9f126af91429
Adrian Armesto <armestomena@gmail.com>
2022-07-16 13:23:31 +0200

    Created function to create an array of neightbours"

--- js/GameOfLIfe/NeigthboursArrayGenerator.js
+++ js/GameOfLIfe/NeigthboursArrayGenerator.js
@@ -1,44 +1,36 @@
-const neightboursArrayGenerator = (habitat, row, column) => {
+const neightboursArrayGenerator = (row, column, habitat, NumberOfRows) => {
+  const neightbours = [];
+
+  if (row === 0) {
+    const Left = habitat[row][column - 1];
+    const Right = habitat[row][column + 1];
+    const RightDown = habitat[row + 1][column + 1];
+    const Down = habitat[row + 1][column];
+    const DownLeft = habitat[row + 1][column - 1];
+    neightbours.push(Left, Right, RightDown, Down, DownLeft);
+    return neightbours;
+  }
+
+  if (row + 1 === NumberOfRows) {
+    const Left = habitat[row][column - 1];
+    const Right = habitat[row][column + 1];
+    const LeftUp = habitat[row - 1][column - 1];
+    const Up = habitat[row - 1][column];
+    const UpRight = habitat[row - 1][column + 1];
+    neightbours.push(Left, LeftUp, Up, UpRight, Right);
+    return neightbours;
+  }
+
   const Left = habitat[row][column - 1];
-  const LeftUp = habitat[row - 1][column - 1];
-  const Up = habitat[row - 1][column];
-  const UpRight = habitat[row - 1][column + 1];
   const Right = habitat[row][column + 1];
   const RightDown = habitat[row + 1][column + 1];
   const Down = habitat[row + 1][column];
   const DownLeft = habitat[row + 1][column - 1];
-  const neightbours = [];
+  const LeftUp = habitat[row - 1][column - 1];
+  const Up = habitat[row - 1][column];
+  const UpRight = habitat[row - 1][column + 1];
 
   neightbours.push(Left, LeftUp, Up, UpRight, Right, RightDown, Down, DownLeft);
 
-  if (row === 0) {
-    neightbours.push(Left, Right, RightDown, Down, DownLeft);
-  }
-  if (row >= habitat.lenght - 1)
-    neightbours.push(
-      Left,
-      LeftUp,
-      Up,
-      UpRight,
-      Right,
-      RightDown,
-      Down,
-      DownLeft
-    );
   return neightbours;
 };
-
-const expectedResult = [
-  [1, 1, 1, 1, 1, 1],
-  [1, 1, 1, 1, 1, 1],
-  [1, 1, 1, 1, 1, 1],
-  [1, 1, 1, 1, 1, 1],
-  [1, 1, 1, 1, 1, 1],
-  [1, 1, 1, 1, 1, 1],
-  [1, 1, 1, 1, 1, 1],
-  [1, 1, 1, 1, 1, 1],
-  [1, 1, 1, 1, 1, 1],
-  [1, 1, 1, 1, 1, 1],
-];
-
-console.log(neightboursArrayGenerator(expectedResult, 4, 1));
