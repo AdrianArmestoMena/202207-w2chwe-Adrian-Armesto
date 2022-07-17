42d15ba6d9d26e1d3808c95087946a1ec6c45e6d
Adrian Armesto <armestomena@gmail.com>
2022-07-17 16:11:29 +0200

    Created main function with interval

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,9 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    jest: true,
   },
-  extends: ["airbnb-base", "pretter"],
+  extends: ["airbnb-base", "prettier"],
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
