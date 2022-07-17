1e7bfa57831b8c1604157e0d7d66a0625645c1a8
Adrian Armesto <armestomena@gmail.com>
2022-07-17 12:51:04 +0200

    Created grid with alive cells

--- js/index.js
+++ js/index.js
@@ -5,14 +5,25 @@ import GenerateLivedCells from "./GameOfLIfe/GenerateLivedCells.js";
 const gridWithOutLife = HabitatArrayGenerator(20, 20);
 const gridWithLife = GenerateLivedCells(gridWithOutLife);
 
-gridWithLife.forEach((row) => {
-  const main = document.getElementById("grid");
-  const newRow = document.createElement("div");
-  newRow.setAttribute("id", "grid-row");
-  main.appendChild(newRow);
-  row.forEach((cell) => {
-    const newcell = document.createElement("div");
-    newcell.className = "grid-row__cell";
-    newRow.appendChild(newcell);
+const createdHtmlGrid = () => {
+  gridWithLife.forEach((row) => {
+    const main = document.getElementById("grid");
+    const newRow = document.createElement("div");
+    newRow.setAttribute("id", "grid-row");
+    main.appendChild(newRow);
+
+    row.forEach((cell) => {
+      const newcell = document.createElement("div");
+
+      if (cell === 1) {
+        newcell.className = "grid-row__cell--death";
+      } else {
+        newcell.className = "grid-row__cell--alive";
+      }
+
+      newRow.appendChild(newcell);
+    });
   });
-});
+};
+createdHtmlGrid(gridWithLife);
+setInterval(cellChanger, 1000);
