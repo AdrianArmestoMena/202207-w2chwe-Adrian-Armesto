1f64cb23ed97fb1b778d373ad7517bb57490ab2a
Adrian Armesto <armestomena@gmail.com>
2022-07-17 12:26:46 +0200

    Created the grid with js

--- js/index.js
+++ js/index.js
@@ -2,4 +2,17 @@ import { cellChanger } from "./GameOfLIfe/CellChanger.js";
 import HabitatArrayGenerator from "./GameOfLIfe/HabitatArrayGenerator.js";
 import GenerateLivedCells from "./GameOfLIfe/GenerateLivedCells.js";
 
-cellChanger(GenerateLivedCells(HabitatArrayGenerator(20, 20)));
+const gridWithOutLife = HabitatArrayGenerator(20, 20);
+const gridWithLife = GenerateLivedCells(gridWithOutLife);
+
+gridWithLife.forEach((row) => {
+  const main = document.getElementById("grid");
+  const newRow = document.createElement("div");
+  newRow.setAttribute("id", "grid-row");
+  main.appendChild(newRow);
+  row.forEach((cell) => {
+    const newcell = document.createElement("div");
+    newcell.className = "grid-row__cell";
+    newRow.appendChild(newcell);
+  });
+});
